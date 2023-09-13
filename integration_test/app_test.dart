761e8c8da3e815468af4bdec78f1afd8e4917e05
Matheus Quost <matheusquost@gmail.com>
2023-09-13 06:18:04 -0300

    🧪 Testing the home screen in the integration test.

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,23 +1,18 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:client_control/main.dart' as app;
-void main(){
+
+void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Integration Test', (tester) async{
+  testWidgets('Integration Test', (tester) async {
     app.main();
     await tester.pumpAndSettle();
-    expect(find.text('Menu'), findsNothing);
-    await Future.delayed(Duration(seconds: 2));
-    await tester.tap(find.byIcon(Icons.menu));
-    await Future.delayed(Duration(seconds: 2));
-    await tester.pumpAndSettle();
-    expect(find.text('Menu'), findsOneWidget);
-    await Future.delayed(Duration(seconds: 2));
+
+    //Testando tela inicial
+    expect(find.text('Clientes'), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.byType(FloatingActionButton), findsOneWidget);
   });
-
-
 }
